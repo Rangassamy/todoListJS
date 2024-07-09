f7c03a5abf6890ce24b27493e073a9543c23d852
Rangassamy <enzorangassamy@gmail.com>
2024-07-09 12:27:37 -0400

    Added a function to store Tasks in Local Storage

--- index.js
+++ index.js
@@ -2,20 +2,35 @@
 const todoForm = document.getElementById("todoForm");
 const todoInput = document.getElementById("todoInput");
 const todoList = document.getElementById("todo");
+const Tasks = [];
 
 // Fonction qui écoute lorque le formulaire est valider
 todoForm.addEventListener("submit", (e) => {
-    // empêche le rechargement de la page
+  // empêche le rechargement de la page
   e.preventDefault();
 
   // Permet d'éviter les beugs lorsque l'utilisateur ne met rien dans l'input
   if (todoInput.value === "") {
     return alert("Veuillez saisir une tâche avant de valider !");
   }
+  Tasks.push(todoInput.value);
+  localStorage.setItem("task", JSON.stringify(Tasks));
+  renderTaskTodo(todoInput.value);
+});
 
-  //On incrémente un li dans l'ul avec la valeur de l'input lorsque le formulaire est valider
-  todo.innerHTML += ` <li>${todoInput.value}</li>`;
-
+//fonction permettant de afficher la valeur dans li
+function renderTaskTodo(value) {
+  todo.innerHTML += ` <li class="todoTask">${value}</li>`;
   // On remet la valeur de l'input à zéro, des guillemets vide pour signaler que il ne doit rien avoir
   todoInput.value = "";
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  renderTaskLocalStorage(JSON.parse(localStorage.getItem("task")));
 });
+
+function renderTaskLocalStorage(values) {
+  values.forEach((value) => {
+    todo.innerHTML += ` <li class="todoTask">${value}</li>`;
+  });
+}
